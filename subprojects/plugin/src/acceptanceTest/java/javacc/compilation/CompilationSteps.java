b2e3c82a10f09536015e4b2923911c61ad6415b7
John Martel <jonathan.martel@coglinc.ca>
2016-11-25 12:43:43 -0500

    Move acceptanceTest subproject to a sourceSet
    The initial purpose of having different subprojects was to ease
    development in IntelliJ which was not dealing cleanly with multiple test
    source sets. Since this is fixed in Intellij 16, this is not required
    anymore.

--- subprojects/plugin/src/acceptanceTest/java/javacc/compilation/CompilationSteps.java
+++ subprojects/plugin/src/acceptanceTest/java/javacc/compilation/CompilationSteps.java
@@ -4,20 +4,15 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.io.Charsets;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class CompilationSteps {
     private File projectDirectory;
@@ -33,17 +28,7 @@ public class CompilationSteps {
 
         projectDirectory = new File(resource.toURI());
 
-        InputStream pluginClasspathStream = getClass().getClassLoader().getResourceAsStream("plugin-classpath.txt");
-        if (pluginClasspathStream == null) {
-            throw new IllegalStateException("Did not find plugin classpath resource, run `testClasses` build task.");
-        }
-        List<File> pluginClasspath = new ArrayList<File>();
-        List<String> pluginClasspathLines = IOUtils.readLines(pluginClasspathStream, Charsets.UTF_8.toString());
-        for (String classpathItem : pluginClasspathLines) {
-            pluginClasspath.add(new File(classpathItem));
-        }
-
-        buildRunner = GradleRunner.create().withProjectDir(projectDirectory).withPluginClasspath(pluginClasspath);
+        buildRunner = GradleRunner.create().withProjectDir(projectDirectory).withPluginClasspath();
     }
 
     private void ensureGivens() {
@@ -51,21 +36,21 @@ public class CompilationSteps {
             throw new IllegalStateException("JavaCC compilation steps assume a project exists. Use givenAProjectNamed(String projectName) first");
         }
     }
-    
+
     public CompilationSteps withArguments(String... arguments) {
         ensureGivens();
-        
+
         String[] defaultArguments = new String[] {
             "--info", "--project-dir", projectDirectory.getAbsolutePath(), "-b", "build.gradle"
         };
-        
+
         String[] allArguments = ArrayUtils.addAll(defaultArguments, arguments);
         buildRunner.withArguments(allArguments);
         buildRunner.forwardOutput();
 
         return this;
     }
-    
+
     public void execute() {
         ensureGivens();
 
@@ -89,7 +74,7 @@ public class CompilationSteps {
     public void andAssertFileExistsButWasNotGenerated(String filename) throws IOException {
         File javaFile = new File(outputDirectory, filename);
         assertTrue(javaFile.exists());
-        
+
         String fileContent = FileUtils.readFileToString(javaFile);
         assertTrue(fileContent.contains("public static final boolean IS_CUSTOM = true;"));
     }
